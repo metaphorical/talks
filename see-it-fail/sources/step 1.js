a4e70b0603164bc064236d41193b2ed5acb9d716
Rastko Vukasinovic <rastko.vukasinovic@gmail.com>
2015-05-20 11:37:56 +0200

    See it fail in English

---
+++ see-it-fail/sources/step 1.js
@@ -0,0 +1,14 @@
+describe('API Call method', function () {
+
+                    it ('should return data object on response success', function (done) {
+                    });
+
+                    it ('should return error object on 400/500 error', function (done) {
+                    });
+
+                    it ('should trigger error logging on 400/500 error', function (done) {
+                    });
+
+                    it ('should trigger error logging with error string', function (done) {
+                    });
+});
