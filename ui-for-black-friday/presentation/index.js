9a4e77bce15923a7a6adcd754fb02dbcfc1e9630
Rastko Vukašinović <rastko.vukasinovic@gmail.com>
2018-06-03 00:35:33 +0200

    Work on ui for black friday deck

--- ui-for-black-friday/presentation/index.js
+++ ui-for-black-friday/presentation/index.js
@@ -12,7 +12,8 @@ import {
   Quote,
   Slide,
   Text,
-  Image
+  Image,
+  CodePane
 } from "spectacle";
 
 // Import theme
@@ -31,6 +32,12 @@ const theme = createTheme({
   secondary: "Helvetica"
 });
 
+const images = {
+  pdp: require("../assets/PDP.png"),
+  rendering: require("../assets/rendering.png"),
+  theline: require("../assets/YEEEEZ.jpg")
+};
+
 export default class Presentation extends React.Component {
   render() {
     return (
@@ -42,15 +49,48 @@ export default class Presentation extends React.Component {
           <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
             ///
           </Text>
+          <Text margin="10px 0 0" textColor="secondary" size={1} bold>
+          Rastko Vukasinovic
+          </Text>
+          <Text margin="10px 0 0" textColor="secondary" size={1} bold>
+          Senior Solution Architect @ Adidas
+          </Text>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            ///
+          </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
           <Text size={6} textColor="secondary">{'\u00A0\u00A0\u00A0\u00A0\u00A0'}///</Text>
           <Heading size={6} textColor="primary" caps>Why React</Heading>
           <Text size={6} textColor="secondary">///</Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Image src={"..assets/PDP.png"} />
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Text size={6} textColor="secondary">{'\u00A0\u00A0\u00A0\u00A0\u00A0'}///</Text>
+          <Heading size={6} textColor="primary" >JSX - native virtual DOM</Heading>
+          <Text size={6} textColor="secondary">///</Text>
         </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <CodePane 
+            source="<ul class='list'>
+                      results.map((item) => <li>{item.name}</li>)
+                    </ul>"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Image src={images.pdp.replace('/', '')} margin="-25px auto" width="25% !important" />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+        <Text size={6} textColor="secondary">{'\u00A0\u00A0\u00A0\u00A0\u00A0'}///</Text>
+        <Heading size={6} textColor="secondary">As it gets more complex...</Heading>
+        <Text size={6} textColor="secondary">///</Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Image src={images.theline.replace('/', '')} margin="-25px auto"  />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Image src={images.rendering.replace('/', '')} margin="-25px auto" />
+        </Slide>
+        
       </Deck>
     );
   }
